6bf3ca53ea8f1200f1bd5b68e1ce97424234fec7
kelvin181 <158399222+kelvin181@users.noreply.github.com>
2025-07-04 18:23:14 +1200

    [3350] [Time Beats: 44.34%] [Memory Beats: 83.96%] - LeetPush

--- LeetCode/Adjacent-Increasing-Subarrays-Detection-II.py
+++ LeetCode/Adjacent-Increasing-Subarrays-Detection-II.py
@@ -1,24 +1,15 @@
 class Solution:
     def maxIncreasingSubarrays(self, arr: List[int]) -> int:
-        # find lengths of increasing subarrays
-        lengths = []
         current_length = 1
+        previous_length = 1
+        res = 1
         
         for i in range(1, len(arr)):
             if arr[i] > arr[i - 1]:
                 current_length += 1
             else:
-                lengths.append(current_length)
+                previous_length = current_length
                 current_length = 1
-        
-        lengths.append(current_length)
-        
-        # check pairs of adjacent increasing subarrays
-        res = 0
-        for i in range(len(lengths) - 1):
-            res = max(res, lengths[i] // 2)
-            res = max(res, min(lengths[i], lengths[i + 1]))
-        
-        res = max(res, lengths[-1] // 2)
+            res = max(res, current_length // 2, min(current_length, previous_length))
 
         return res
