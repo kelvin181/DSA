200e566e3ae4855ed993f73f61ab21a1682390be
kelvin181 <158399222+kelvin181@users.noreply.github.com>
2025-09-20 13:43:28 +1200

    [3508] [Time Beats: 10.08%] [Memory Beats: 15.59%] - LeetPush

--- LeetCode/Implement-Router.py
+++ LeetCode/Implement-Router.py
@@ -1,76 +1,65 @@
-# q for current router
-# set for packets in current router
-# hashmap of queues that store sorted times for each destination
-# binary search for count
-
 class Router:
 
     def __init__(self, memoryLimit: int):
-        self.q = deque()
-        self.packets = set()
-        self.destinations = {}
         self.limit = memoryLimit
-        self.length = 0
+        self.q = deque()
+        self.items = set()
+        self.destinations = {}
 
-    def addPacket(self, source: int, destination: int, timestamp: int) -> bool:
-        if tuple([source, destination, timestamp]) in self.packets:
+    def addPacket(self, s: int, d: int, t: int) -> bool:
+        if (s, d, t) in self.items:
             return False
-        if self.length == self.limit:
-            s, d, t = self.q.popleft()
-            self.packets.remove((s, d, t))
-            self.destinations[d].popleft()
-            self.length -= 1
-        self.q.append((source, destination, timestamp))
-        self.packets.add((source, destination, timestamp))
-        if destination not in self.destinations:
-            self.destinations[destination] = deque()
-        self.destinations[destination].append(timestamp)
-        self.length += 1
+        if len(self.items) == self.limit:
+            removed = self.q.popleft()
+            self.items.remove(removed)
+            self.destinations[removed[1]].popleft()
+        self.q.append((s, d, t))
+        self.items.add((s, d, t))
+        if d not in self.destinations:
+            self.destinations[d] = deque()
+        self.destinations[d].append(t)
         return True
 
     def forwardPacket(self) -> List[int]:
-        if self.length == 0:
+        if not self.q:
             return []
         s, d, t = self.q.popleft()
-        self.packets.remove((s, d, t))
+        self.items.remove((s, d, t))
         self.destinations[d].popleft()
-        self.length -= 1
         return [s, d, t]
 
-    def getCount(self, destination: int, startTime: int, endTime: int) -> int:
-
-        times = self.destinations.get(destination, [])
-
-        if not times:
+    def getCount(self, d: int, s: int, e: int) -> int:
+        if d not in self.destinations:
             return 0
-
-        # search first number >= startTime
+            
+        arr = self.destinations[d]
         left = inf
+        right = -inf
+
+        # first greater than or equal to s 
         l = 0
-        r = len(times) - 1
+        r = len(arr) - 1
         while l <= r:
             m = l + (r - l) // 2
-            if times[m] < startTime:
-                l = m + 1
-            else:
+            if arr[m] >= s:
                 left = min(left, m)
                 r = m - 1
-
-        # search first number <= endTime
-        right = -inf
-        l = 0
-        r = len(times) - 1
-        while l <= r:
-            m = l + (r - l) // 2
-            if times[m] > endTime:
-                r = m - 1
             else:
-                right = max(right, m)
                 l = m + 1
         
-        if right == -inf or left == inf:
-            return 0
-        return right - left + 1
+        # first less than or equal to e
+        l = 0
+        r = len(arr) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            if arr[m] <= e:
+                right = max(right, m)
+                l = m + 1
+            else:
+                r = m - 1
+
+        return max(0, right - left + 1)
+
 
 # Your Router object will be instantiated and called as such:
 # obj = Router(memoryLimit)
