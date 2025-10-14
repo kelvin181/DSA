7a1d3784189afbe69d60b3356e798037ab40f69c
kelvin181 <158399222+kelvin181@users.noreply.github.com>
2025-10-14 15:55:40 +1300

    [3349] [Time Beats: 79.14%] [Memory Beats: 98.40%] - LeetPush

--- LeetCode/Adjacent-Increasing-Subarrays-Detection-I.py
+++ LeetCode/Adjacent-Increasing-Subarrays-Detection-I.py
@@ -1,18 +1,15 @@
 class Solution:
     def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
-        q = deque()
-        valid = [False] * len(nums)
+        prev = 0
+        curr = 1
 
-        for i, num in enumerate(nums):
-            while q and q[-1][1] >= num:
-                q.pop()
-            q.append((i, num))
-            while q and q[0][0] < i - k + 1:
-                q.popleft()
-            if len(q) == k:
-                valid[i] = True
-
-        for i in range(len(nums) - 1, (2 * k) - 2, -1):
-            if valid[i] and valid[i - k]:
+        for i in range(1, len(nums)):
+            if nums[i - 1] < nums[i]:
+                curr += 1
+            else:
+                prev, curr = curr, 1
+            
+            if curr >= k and prev >= k or curr == 2 * k:
                 return True
+        
         return False
